b4c24f20822d5346a639c7f17d75aba2d46d1a1d
Perry Chou <906135+mbcoguno@users.noreply.github.com>
2025-10-18 22:12:27 +0800

    feat: check specific device exist

--- files/system/usr/bin/after-sleep.sh
+++ files/system/usr/bin/after-sleep.sh
@@ -2,9 +2,11 @@
 
 set -euo pipefail
 
-echo 1 > /sys/bus/pci/devices/0000:c3:00.4/reset
-# echo 1 > /sys/devices/pci0000:00/0000:00:08.1/0000:c3:00.4/reset
-echo 1 > /sys/bus/pci/devices/0000:c3:00.4/remove
-# echo 1 > /sys/devices/pci0000:00/0000:00:08.1/0000:c3:00.4/remove
+if [ -d "/sys/devices/pci0000:00/0000:00:08.1/0000:c3:00.4/" ]; then
+  echo 1 > /sys/bus/pci/devices/0000:c3:00.4/reset
+  # echo 1 > /sys/devices/pci0000:00/0000:00:08.1/0000:c3:00.4/reset
+  echo 1 > /sys/bus/pci/devices/0000:c3:00.4/remove
+  # echo 1 > /sys/devices/pci0000:00/0000:00:08.1/0000:c3:00.4/remove
 
-echo 1 > /sys/bus/pci/rescan
+  echo 1 > /sys/bus/pci/rescan
+fi
